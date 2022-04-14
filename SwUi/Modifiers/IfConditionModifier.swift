b224a74349095e6dfee1a4a2bffddf7d82b21968
Viktor Berezhnytskyi <vberezhnytskyi@perpet.io>
2022-04-14 15:41:18 +0300

    Added: IfConditionModifier

---
+++ SwUi/Modifiers/IfConditionModifier.swift
@@ -0,0 +1,55 @@
+//
+//  IfConditionModifier.swift
+//  SwUi
+//
+//  Created by Viktor Berezhnytskyi on 14.04.2022.
+//
+
+import SwiftUI
+
+struct IfConditionModifier: View {
+    
+    @State var isTrue: Bool = true
+    
+    var body: some View {
+        VStack {
+            Text("Hello, World! 1")
+                .ifCondition(isTrue) { $0.bold() }
+            
+            Text("Hello, World! 2")
+                .ifCondition(isTrue) { view in
+                    view
+                        .bold()
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                        .padding(.leading, 20)
+                }
+            
+            Button("Toggle") {
+                isTrue.toggle()
+            }
+            .buttonStyle(BorderedButtonStyle())
+        }
+    }
+}
+
+// Apply some modifiers if condition == true, else return self view.
+extension View {
+
+    @ViewBuilder
+    func ifCondition<T>(_ condition: Bool, view: (Self) -> T) -> some View where T: View {
+            if condition {
+                view(self)
+            } else {
+                self
+            }
+    }
+}
+
+#if DEBUG
+struct IfConditionModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        IfConditionModifier()
+    }
+}
+#endif
