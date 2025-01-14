35764a7b3557e353a81f95f2b63e26e239c77954
Viktor Berezhnytskyi <vberezhnytskyi@perpet.io>
2025-01-14 13:28:57 +0200

    add sensoryFeedback extension

--- SwUi/Extensions/Extension+View.swift
+++ SwUi/Extensions/Extension+View.swift
@@ -21,3 +21,44 @@ extension View {
         self.animation(nil, value: UUID())
     }
 }
+
+// MARK: - sensoryFeedback
+@available(iOS 17.0, *)
+extension View {
+    func sensoryFeedback<T>(
+        _ feedback: SensoryFeedback,
+        trigger: T,
+        equal: T
+    ) -> some View where T : Equatable {
+        self.sensoryFeedback(feedback, trigger: trigger) { _, newValue in
+            newValue == equal
+        }
+    }
+}
+
+// Usage
+
+/*
+ struct UsageView: View {
+     
+     @State private var isPresented: Bool = false
+     @State private var number: Int = 0
+     
+     var body: some View {
+         VStack(spacing: 16) {
+             
+             /// Fire when isPresented == true
+             Button("Done") {
+                 isPresented.toggle()
+             }
+             .sensoryFeedback(.impact, trigger: isPresented, equal: true)
+             
+             /// Fire when number == 5
+             Button("+") {
+                 number += 1
+             }
+             .sensoryFeedback(.impact, trigger: number, equal: 5)
+         }
+     }
+ }
+ */
