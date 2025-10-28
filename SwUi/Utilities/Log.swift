3403585b898259f4efa807177ba6975022dc58ce
Viktor Berezhnytskyi <vberezhnytskyi@perpet.io>
2025-10-28 13:55:44 +0200

    Add: UserDefaultsDB Update: Log

--- SwUi/Utilities/Log.swift
+++ SwUi/Utilities/Log.swift
@@ -18,6 +18,43 @@ public enum LogType {
     case dump(name: String? = nil, indent: Int = 0, maxDepth: Int = .max, maxItems: Int = .max)
 }
 
+/// Logs messages to the console with detailed metadata including file, line, function, and timestamp.
+///
+/// The `Log` function supports two logging types: simple `print` and structured `dump`.
+/// It also automatically categorizes logs based on whether the first item is an `Error`,
+/// and adjusts the log state accordingly.
+///
+/// - Parameters:
+///   - items: A variadic list of items to log. Can include any type. If the first item is an `Error`, the log will be marked as `.error`.
+///   - fileID: The file identifier where the log was called. Defaults to the last path component of `#fileID`.
+///   - line: The line number where the log was invoked. Automatically captured using `#line`.
+///   - funcName: The function name from which the log was called. Captured using `#function`.
+///   - type: The logging output type, either `.print` for flat logging or `.dump` for detailed structure output.
+///   - state: The log severity level, such as `✅ .info`, `⚠️ .warning`, or `‼️ .error`. Automatically set to `.error` if the first `item` is an `Error`.
+///
+/// - Important:
+///   - `fileID`, `line` and `funcName` captured automatically and should not be set manually.
+///   - For iOS 15+ and macOS 12+, the timestamp uses a localized time-only format. On earlier versions, the full `Date.description` is used.
+///
+/// - Example:
+/// ```swift
+/// func some() {
+///     Log("Hello")
+///     Log("Hello", state: .warning)
+///     Log("Hello", state: .error) // Will auto-detect as .error
+///     Log("Hello", type: .dump())
+/// }
+///
+/// /*
+/// Terminal:
+/// ✅ Log.swift ⏰ 4:22:33 PM #️⃣ 43 some() ⏩ Hello
+/// ⚠️ Log.swift ⏰ 4:22:33 PM #️⃣ 44 some() ⏩ Hello
+/// ‼️ Log.swift ⏰ 4:22:33 PM #️⃣ 45 some() ⏩ Hello
+/// ✅ Log.swift ⏰ 4:22:33 PM #️⃣ 45 some() ⏩ DUMP ⬇️
+/// ▿ 1 element
+///   - "Hello"
+/// */
+/// ```
 public func Log(
     _ items: Any...,
     fileID: String = #fileID,
@@ -45,21 +82,3 @@ public func Log(
     }
 #endif
 }
-
-/*
- 
- func some() {
-     Log("Hello")
-     Log("Hello", state: .warning)
-     Log("Hello", state: .error)
-     Log("Hello", type: .dump())
- }
- 
- Terminal:
- ✅ Log.swift ⏰ 4:22:33 PM #️⃣ 43 some() ⏩ Hello
- ⚠️ Log.swift ⏰ 4:22:33 PM #️⃣ 44 some() ⏩ Hello
- ‼️ Log.swift ⏰ 4:22:33 PM #️⃣ 45 some() ⏩ Hello
- ✅ Log.swift ⏰ 4:22:33 PM #️⃣ 45 some() ⏩ DUMP ⬇️
- ▿ 1 element
-   - "Hello"
- */
---
+++ SwUi/Utilities/UserDefaults/UserDefaultsDB.swift
@@ -0,0 +1,88 @@
+//
+//  UserDefaultsDB.swift
+//  SwUi
+//
+//  Created by Viktor Berezhnytskyi on 28.10.2025.
+//
+
+import Foundation
+
+// Better to use when object key is very important, and need save values without mistake
+// Better to use when create Local DB.
+// If you do not need separated suit, remove suiteName from init and replace `defaults` on UserDefaults.standard
+
+protocol DBKeyed {
+    static var dbKey: String { get }
+}
+
+@propertyWrapper
+fileprivate struct UserDefaultsDB<T: Codable> {
+    private let defaults: UserDefaults?
+    private let dbKey: String
+    private let defaultValue: T
+    
+    /// For case when `T` is NOT Optional
+    init(suiteName: String, defaultValue: T) where T: Codable & DBKeyed {
+        self.defaults = UserDefaults(suiteName: suiteName)
+        self.dbKey = T.dbKey
+        self.defaultValue = defaultValue
+    }
+    
+    /// For case when `T` is Optional
+    init<V>(suiteName: String, defaultValue: T) where V: Codable & DBKeyed, T == Optional<V> {
+        self.defaults = UserDefaults(suiteName: suiteName)
+        self.dbKey = V.dbKey
+        self.defaultValue = defaultValue
+    }
+    
+    /// For case when `T` is Sequence
+    init(suiteName: String, defaultValue: T) where T: Sequence, T.Element: Codable & DBKeyed {
+        self.defaults = UserDefaults(suiteName: suiteName)
+        self.dbKey = T.Element.dbKey
+        self.defaultValue = defaultValue
+    }
+    
+    /// For case when `T` is Dictionary
+    init<K, V>(suiteName: String, defaultValue: T) where T: Codable, K: Hashable, V: Codable & DBKeyed, T == Dictionary<K, V> {
+        self.defaults = UserDefaults(suiteName: suiteName)
+        self.dbKey = V.dbKey
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            guard
+                let defaults,
+                let data = defaults.object(forKey: dbKey) as? Data,
+                let value = try? JSONDecoder().decode(T.self, from: data)
+            else { return defaultValue }
+            
+            return value
+        }
+        set {
+            if let encoded = try? JSONEncoder().encode(newValue) {
+                defaults?.set(encoded, forKey: dbKey)
+            }
+        }
+    }
+}
+
+// MARK: - Usage
+
+/*
+ struct UserModel {
+     let name: String
+     let age: Int
+ }
+
+ extension UserModel: DBKeyed {
+     static var dbKey: String {
+         "UserModelDBKey"
+     }
+ }
+
+ static let suiteName: String = "com.suiteName"
+
+ @UserDefaultsDB(suiteName: suiteName, defaultValue: nil)
+ private var user: UserModel?
+ */
